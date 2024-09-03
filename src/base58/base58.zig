997841f3d79c5b847a13258401c3b9971fbd7b8c
StringNick <stringnickq@gmail.com>
2024-09-03 10:28:40 +0200

    base58 tests (default and with checksum)

--- src/base58/base58.zig
+++ src/base58/base58.zig
@@ -1,3 +1,7 @@
 pub const Encoder = @import("encode.zig").Encoder;
 pub const Decoder = @import("decode.zig").Decoder;
 pub const Alphabet = @import("alphabet.zig").Alphabet;
+
+test {
+    _ = @import("tests.zig");
+}
